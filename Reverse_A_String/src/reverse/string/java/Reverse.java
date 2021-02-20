d24cc5abd8315f9a58f653cb5302ecff1b060dfe
HP <HP@DESKTOP-TS9LEQP>
2021-02-20 08:24:41 -0800

    commit

---
+++ Reverse_A_String/src/reverse/string/java/Reverse.java
@@ -0,0 +1,19 @@
+package reverse.string.java;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string1="java full stack development";
+		char ch[]=string1.toCharArray();
+		String rev="";
+		for(int i=ch.length-1;i>=0;i--)
+		{
+			rev=rev+ch[i];
+		}
+		System.out.println(rev);
+		
+
+	}
+
+}
